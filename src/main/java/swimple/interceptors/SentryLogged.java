69568cb65929a699e8a606bebb733df321ebf928
Tom Bakker <tom.bakker@truetrack.nl>
2019-05-02 17:09:37 +0200

    Sentry Logging

---
+++ src/main/java/swimple/interceptors/SentryLogged.java
@@ -0,0 +1,11 @@
+package swimple.interceptors;
+
+import javax.interceptor.InterceptorBinding;
+import java.lang.annotation.*;
+
+@Inherited
+@InterceptorBinding
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface SentryLogged {
+}
